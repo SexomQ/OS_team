d9c45ad5f08e443f7a85baac8dfe87dfd31479e7
SexomQ <sclifos-tudor@mail.ru>
2023-11-26 22:22:43 +0200

    minor changes

--- lab3/tasks/floppy_to_ram.asm
+++ lab3/tasks/floppy_to_ram.asm
@@ -9,7 +9,7 @@ menu_handle_floppy_ram:
     mov si, conversion_buffer
     call string_to_int
     mov byte [head], al
-s
+   
     call clear_current_row
 
     ; get the cylinder
@@ -31,7 +31,7 @@ s
     mov si, conversion_buffer
     call string_to_int
     mov byte [sector], al
-
+ 
     call clear_current_row
 
     ; get the number of sectors
@@ -53,40 +53,65 @@ s
     mov si, conversion_buffer
     call string_to_hex
     mov [ram_address], ax
-
+  
     call clear_current_row
 
-    call read_floppy_ram
+    mov ah, 0x02
+    mov dh, 0x00
+    mov dl, 0x00
+    int 0x10
+
+    mov ah, 0x0e
+    mov al, 'R'
+    int 0x10
+
+    jmp read_floppy_ram
+
+print_ram:
+    mov cx, 0
+    mov bx, 0x2000
+    mov es, bx
+    mov bx, 0x0000
     call print_str_from_ram
 
-
-    
-
-    ; jmp menu
+    jmp menu
 
 read_floppy_ram:
     ; Set up the RAM address to store the data
-    mov bx, 7c00h  ; RAM address to store the data
+    mov bx, 0x2000  ; RAM address to store the data
     mov es, bx  ; Set the segment register to the RAM address
     mov bx, 0x0000  ; Offset to the RAM address
 
     ; Set up the floppy disk parameters
     mov ah, 0x02  ; Read sector function
-    mov al, [number_of_sectors]  ; Number of sectors to read
-    mov ch, [cylinder]  ; Cylinder number
-    mov cl, [sector]  ; Sector number
-    mov dh, [head]  ; Head number
+    mov al, number_of_sectors  ; Number of sectors to read
+    mov ch, cylinder  ; Cylinder number
+    mov cl, sector  ; Sector number
+    mov dh, head  ; Head number
     mov dl, [BOOT_DISK]  ; Drive number (0x00 for floppy disk)
 
     int 0x13  ; BIOS interrupt
-    ret
+
+    mov ax, 0x2000
+    mov ds, ax
+    mov es, ax
+    mov ss, ax
+    jmp print_ram
     
 print_str_from_ram:
-    mov si, [es:bx]
-    mov bh, 0
-    mov bl, 07H
-    mov dh, 0
-    mov dl, 0
-    call print_string   
 
-    ret
+    cmp cx, 512
+    je .stop_printing
+
+    mov al, [es:bx]
+    mov ah, 0eh
+    int 10h
+
+    inc cx
+    inc bx
+    jmp print_str_from_ram
+
+
+    .stop_printing:
+
+        ret
